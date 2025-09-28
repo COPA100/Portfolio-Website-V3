eb88e183e8182e8ade1a63cb28589098a0910e0a
Colin Park <colinpark100@gmail.com>
2025-09-28 01:48:15 -0400

    changed experience data

--- my-project/src/data/about.js
+++ my-project/src/data/about.js
@@ -1,15 +1,15 @@
 const about = [
     {
         id:1,
-        text:"I am a first-year at the University of Virginia receiving my B.S. in Computer Science in May 2029. I have a passion for web development and enjoy creating aesthetic and functional websites that provide exceptional user experiences.",
+        text:"I'm a first-year student at the University of Virginia, pursuing a BS in Computer Science and graduating in May 2029. I specialize in frontend development and love building websites that look great and work smoothly.",
     },
     {
         id:2,
-        text:"When I’m not coding, I like to spend my time playing golf, pickleball, and poker, as well as learning guitar.",
+        text:"Recently, I’ve become interested in backend development and want to expand my skills to become a more well-rounded developer. I’m always looking for opportunities to learn, work on cool projects, and connect with others in tech.",
     },
     {
         id:3,
-        text:"I'm excited to grow my skills through internships and collaborative projects while building innovative solutions that make a positive impact.",
+        text:"Outside of coding, I enjoy golf, pickleball, poker, and learning guitar.",
     },
 ];
 
